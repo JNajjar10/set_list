d1bb4f5aca57b1fa48673f74626a63bd5f5b47e0
Jonathan Najjar <jonathannajjar@Jonathans-MacBook-Air.local>
2021-04-04 14:26:32 -0400

    full auth

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module spotifyMusicVideo
 go 1.15
 
 require (
+	github.com/auth0/go-jwt-middleware v1.0.0
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/form3tech-oss/jwt-go v3.2.2+incompatible
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
+	github.com/gin-gonic/gin v1.6.3
 	github.com/gorilla/mux v1.8.0
 	github.com/zmb3/spotify v1.1.1
+	golang.org/x/oauth2 v0.0.0-20210313182246-cd4f82c27b84
 	google.golang.org/api v0.43.0
 )
--- main.go
+++ main.go
@@ -1,26 +1,120 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
-	my_server "spotifyMusicVideo/server"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/form3tech-oss/jwt-go"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
 )
 
-const developerKey = "AIzaSyAqQce_UVA3HC11-7yrlsCB7kJI_b6ALV4"
+type Response struct {
+	Message string `json:"message"`
+}
 
-func main(){
-	//Initialize the server
-	server := my_server.NewServer()
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
 
-	//Setup data
-	my_server.InitialiseData()
-	name, _ := server.SpotifyServer.Client.CurrentUser()
-	my_server.Id = append(my_server.Id, my_server.ID{name.ID})
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
 
-	//Start Server
-	go func() {
-		server.InitialiseRoutes()
-		server.Run()
-	}()
+type Joke struct {
+	ID    int    `json:"id" binding:"required"`
+	Likes int    `json:"likes"`
+	Joke  string `json:"joke" binding:"required"`
+}
+
+/** we'll create a list of jokes */
+var jokes = []Joke{
+	Joke{1, 0, "Did you hear about the restaurant on the moon? Great food, no atmosphere."},
+	Joke{2, 0, "What do you call a fake noodle? An Impasta."},
+	Joke{3, 0, "How many apples grow on a tree? All of them."},
+	Joke{4, 0, "Want to hear a joke about paper? Nevermind it's tearable."},
+	Joke{5, 0, "I just watched a program about beavers. It was the best dam program I've ever seen."},
+	Joke{6, 0, "Why did the coffee file a police report? It got mugged."},
+	Joke{7, 0, "How does a penguin build it's house? Igloos it together."},
+	Joke{8, 0, "Dad, did you get a haircut? No I got them all cut."},
+	Joke{9, 0, "What do you call a Mexican who has lost his car? Carlos."},
+	Joke{10, 0, "Dad, can you put my shoes on? No, I don't think they'll fit me."},
+	Joke{11, 0, "Why did the scarecrow win an award? Because he was outstanding in his field."},
+	Joke{12, 0, "Why don't skeletons ever go trick or treating? Because they have no body to go with."},
+}
+
+var jwtMiddleWare *jwtmiddleware.JWTMiddleware
+
+func main() {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			aud := os.Getenv("AUTH0_API_AUDIENCE")
+			checkAudience := token.Claims.(jwt.MapClaims).VerifyAudience(aud, false)
+			if !checkAudience {
+				return token, errors.New("Invalid audience.")
+			}
+			// verify iss claim
+			iss := os.Getenv("AUTH0_DOMAIN")
+			checkIss := token.Claims.(jwt.MapClaims).VerifyIssuer(iss, false)
+			if !checkIss {
+				return token, errors.New("Invalid issuer.")
+			}
+
+			cert, err := getPemCert(token)
+			if err != nil {
+				log.Fatalf("could not get cert: %+v", err)
+			}
+
+			result, _ := jwt.ParseRSAPublicKeyFromPEM([]byte(cert))
+			return result, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
+	})
+
+	jwtMiddleWare = jwtMiddleware
+	// Set the router as the default one shipped with Gin
+	router := gin.Default()
+
+	// Serve the frontend
+	router.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+	api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "pong",
+			})
+		})
+		api.GET("/jokes", authMiddleware(), JokeHandler)
+		api.POST("/jokes/like/:jokeID", authMiddleware(), LikeJoke)
+	}
+	// Start the app
+	router.Run(":3000")
+	//old stuff
+	////Initialize the server
+	//server := my_server.NewServer()
+	//
+	////Setup data
+	//my_server.InitialiseData()
+	//name, _ := server.SpotifyServer.Client.CurrentUser()
+	//my_server.Id = append(my_server.Id, my_server.ID{name.ID})
+	//
+	////Start Server
+	//go func() {
+	//	server.InitialiseRoutes()
+	//	server.Run()
+	//}()
 
 	//Youtube stuff
 
@@ -28,8 +122,8 @@ func main(){
 	//song, _ := youtube_client.GetSong("Laroi")
 	//fmt.Println(song)
 
-	select {
-	}
+	//select {
+	//}
 }
 
 // Print the ID and title of each result in a list as well as a name that
@@ -42,4 +136,72 @@ func printIDs(sectionName string, matches map[string]string) {
 		fmt.Printf("[%v] %v\n", id, title)
 	}
 	fmt.Printf("\n\n")
+}
+
+func getPemCert(token *jwt.Token) (string, error) {
+	cert := ""
+	resp, err := http.Get(os.Getenv("AUTH0_DOMAIN") + ".well-known/jwks.json")
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	x5c := jwks.Keys[0].X5c
+	for k, v := range x5c {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + v + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		return cert, errors.New("unable to find appropriate key")
+	}
+
+	return cert, nil
+}
+
+// authMiddleware intercepts the requests, and check for a valid jwt token
+func authMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Get the client secret key
+		err := jwtMiddleWare.CheckJWT(c.Writer, c.Request)
+		if err != nil {
+			// Token not found
+			fmt.Println(err)
+			c.Abort()
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("Unauthorized"))
+			return
+		}
+	}
+}
+
+// JokeHandler returns a list of jokes available (in memory)
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+
+	c.JSON(http.StatusOK, jokes)
+}
+
+func LikeJoke(c *gin.Context) {
+	// Check joke ID is valid
+	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		// find joke and increment likes
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes = jokes[i].Likes + 1
+			}
+		}
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		// the jokes ID is invalid
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
